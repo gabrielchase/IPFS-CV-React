74892a3767bef8f11e06679dbb63e6eca5ae0e2b
gabrielchase <gchase.patron@gmail.com>
2018-11-02 20:50:24 +0800

    Constants

---
+++ src/constants.js
@@ -0,0 +1,9 @@
+export const DEGREE_OPTIONS = [
+    { key: 'PhD', value: 'PhD', text: "Doctorate (PhD)" }, 
+    { key: 'MS', value: 'MS', text: "Master's Degree (MS)" }, 
+    { key: 'MA', value: 'MA', text: "Master's Degree (MA)" }, 
+    { key: 'MBA', value: 'MBA', text: "Master's Degree (MBA)" }, 
+    { key: 'BS', value: 'BS', text: "Bachelor's Degree (BS)" }, 
+    { key: 'BA', value: 'BA', text: "Bachelor's Degree (BA)" }, 
+    { key: 'HS Diploma', value: 'HS Diploma', text: "High School Diploma" }, 
+]

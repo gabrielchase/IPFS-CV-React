5398aea68b9364a55ccbb1c5e09b76d31f5fbee8
gabrielchase <gchase.patron@gmail.com>
2018-11-02 22:19:31 +0800

    Comment

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import { render } from 'react-dom'
 import { Provider } from 'react-redux'
 import { Switch, Route } from 'react-router'
 import { ConnectedRouter } from 'connected-react-router'
-import { Redirect } from 'react-router-dom'
+// import { Redirect } from 'react-router-dom'
 
 import store, { history } from './store'
 
@@ -13,24 +13,24 @@ import serviceWorker from './serviceWorker'
 import Login from './components/login'
 import Dashboard from './components/dashboard'
 
-const verifyJWT = () => {
-    console.log('verifying jwt')
-    let exp = localStorage.getItem('token_exp')
-    exp = parseInt(exp)
+// const verifyJWT = () => {
+//     console.log('verifying jwt')
+//     let exp = localStorage.getItem('token_exp')
+//     exp = parseInt(exp)
     
-    let current_time = Math.floor(Date.now() / 1000)
-    current_time = parseInt(current_time)
+//     let current_time = Math.floor(Date.now() / 1000)
+//     current_time = parseInt(current_time)
 
-    if (current_time > exp) {
-        localStorage.removeItem('token')
-        localStorage.removeItem('token_exp')
-        console.log('returning false')
-        return false 
-    }
+//     if (current_time > exp) {
+//         localStorage.removeItem('token')
+//         localStorage.removeItem('token_exp')
+//         console.log('returning false')
+//         return false 
+//     }
     
-    if (current_time <= exp )
-        return true 
-}
+//     if (current_time <= exp )
+//         return true 
+// }
 
 // const PrivateRoute = ({ component: Component, ...rest }) => (
 //     <Route

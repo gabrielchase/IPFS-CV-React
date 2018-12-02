e85fd03ad9927a704caba0478b4df327efc209fc
gabrielchase <gchase.patron@gmail.com>
2018-12-02 21:16:11 +0800

    Styling on cv view

--- src/components/cv_view.js
+++ src/components/cv_view.js
@@ -6,52 +6,54 @@ export const CvView = ({ current_user }) => (
         <Container id='cv-preview'>
             <br />
             <Grid centered>
-                <h2>{ current_user.first_name } { current_user.last_name}</h2>
+                <h2 id='top-name'>{ current_user.first_name } { current_user.last_name}</h2>
             </Grid>
             <Grid centered>
-                { current_user.email }
+                <span id='top-email'>{ current_user.email }</span>
             </Grid>
             <br />
             <br />
             <br />
-            <Grid centered>
-                <h3 id='section-title'>Education</h3>
-            </Grid>
-            <br />
-            {
-                current_user.education.map((e, i) => {
-                    return (
-                        <div key={i}>   
-                            <strong id='school'>{e.school}</strong> <span id='date' float='right'>({e.start_date_slug} - {e.end_date_slug})</span>
-                            <br />
-                            <span id='course'>{e.course}</span>
-                            <br />
-                            <br />
-                        </div>
-                    )
-                })
-            }
-            <br />
-            <br />
-            <Grid centered>
-                <h3 id='section-title'>Experience</h3>
-            </Grid>
-            <br />
-            {
-                current_user.experience.map((e, i) => {
-                    return (
-                        <div key={i}>   
-                            <strong id='position'>{e.position}</strong> <span id='date' float='right'>({e.start_date_slug} - {e.end_date_slug})</span>
-                            <br />
-                            <span id='company'>{e.company}</span>
-                            <br />
-                            <span id='description'>{e.description}</span>
-                            <br />
-                            <br />
-                        </div>
-                    )
-                })
-            }
+            <div id='cv-body'>
+                <Grid>
+                    <h3 id='section-title'>EDUCATION</h3>
+                </Grid>
+                <br />
+                {
+                    current_user.education.map((e, i) => {
+                        return (
+                            <div key={i}>   
+                                <strong id='school'>{e.school}</strong> <span id='date' float='right'>({e.start_date_slug} - {e.end_date_slug})</span>
+                                <br />
+                                <span id='course'>{e.course}</span>
+                                <br />
+                                <br />
+                            </div>
+                        )
+                    })
+                }
+                <br />
+                <br />
+                <Grid>
+                    <h3 id='section-title'>EXPERIENCE</h3>
+                </Grid>
+                <br />
+                {
+                    current_user.experience.map((e, i) => {
+                        return (
+                            <div key={i}>   
+                                <strong id='position'>{e.position}</strong> <span id='date' float='right'>({e.start_date_slug} - {e.end_date_slug})</span>
+                                <br />
+                                <span id='company'>{e.company}</span>
+                                <br />
+                                <span id='description'>{e.description}</span>
+                                <br />
+                                <br />
+                            </div>
+                        )
+                    })
+                }
+            </div>
         </Container>
     </Segment>
 )
